df4034949e74dd86222353ab78f9777f561834ad
800-Raph <eminemgees@gmail.com>
2025-01-07 00:04:55 +0000

    Add files via upload

---
+++ RAPH.py
@@ -0,0 +1,7 @@
+# A PROGRAM TO DISPLAY HALLO WORLD AND SOME BASIC ARITHMETIC
+print("Hello World")
+# BASIC ARITHMETIC CALCULATIONS
+print("3 + 5 =", 3 + 5)
+print("10 * 5 =", 10 * 5)
+print("2 / 4 =", 2 / 4)
+print("12 - 6 =", 12 - 6)
